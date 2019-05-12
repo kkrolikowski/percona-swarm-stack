76a870e0836db9183d46af81186088f7869ef921
Krzysztof Królikowski <kkrolikowski@gmail.com>
2019-05-12 11:19:58 +0200

    get IPs of cluster nodes

--- tools/update_sqlproxy.py
+++ tools/update_sqlproxy.py
@@ -12,7 +12,8 @@ etcd = requests.get("http://swarm-node1.lan:2379/v2/keys/pxc-cluster/clsql")
 if etcd.status_code == 200:
     etcd_json = json.loads(etcd.text)
     for nodes in etcd_json['node']['nodes']:
-        print nodes['key']
+        hosts = nodes['key'].split('/')
+        print hosts[3]
 
 try:
     for container in dockerCli.containers.list():
