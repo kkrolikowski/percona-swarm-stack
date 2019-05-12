25675fd1b6e97a5216d0480ff30e3dac5b36adcb
Krzysztof Królikowski <kkrolikowski@gmail.com>
2019-05-12 09:24:00 +0200

    try to read something from etcd

--- tools/update_sqlproxy.py
+++ tools/update_sqlproxy.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 
 import docker
+import etcd
 
 dockerCli = docker.from_env()
+etcdCli = etcd.Client(host='swarm-node1.lan', port='2379')
+
+try:
+    etcdCli.read('/v2/keys/pxc-cluster/clsql', recursive=True)
+except etcd.EtcdKeyNotFound:
+    print "nothing found"
+
 
 try:
     for container in dockerCli.containers.list():
         if "percona_sqlproxy" in container.name:
             ret = container.exec_run("add_cluster_nodes.sh", stdout=False,stderr=False)
-            print ret
             break
 
 except 	docker.errors.APIError:
