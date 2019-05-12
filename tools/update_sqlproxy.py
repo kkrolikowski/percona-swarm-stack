d9ed8b86eeef3f1d36ec328dbab718913d391dd1
Krzysztof Królikowski <kkrolikowski@gmail.com>
2019-05-12 11:13:01 +0200

    parser fix

--- tools/update_sqlproxy.py
+++ tools/update_sqlproxy.py
@@ -12,7 +12,7 @@ etcd = requests.get("http://swarm-node1.lan:2379/v2/keys/pxc-cluster/clsql")
 if etcd.status_code == 200:
     etcd_json = json.loads(etcd.text)
     for nodes in etcd_json['node']['nodes']:
-        print nodes.key
+        print nodes['key']
 
 try:
     for container in dockerCli.containers.list():
